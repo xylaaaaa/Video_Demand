58e56d9284b8f7fdf6dab046a11ccdf4097653dc
user <2392805527@qq.com>
2025-05-14 18:30:19 +0800

    测试httplib

---
+++ http/server.cpp
@@ -0,0 +1,22 @@
+#include "./httplib.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    httplib::Server svr;
+    svr.Get("/hi", [](const httplib::Request &req, httplib::Response &res)
+            {
+        res.set_content("Hello World!", "text/plain");
+        cout<<res.status<<endl;
+        cout<<res.body<<endl;
+        for(auto& x:req.headers)
+        {
+            cout<<x.first<<" : "<<x.second<<endl;
+        }
+        std::string body = "<h1>HelloWorld</h1>";
+        res.set_content(body, "text/html");
+        res.status = 200; });
+    svr.listen("0.0.0.0", 8082);
+    return 0;
+}
